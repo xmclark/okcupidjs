939fcec3a5a1e38a7905b2712f184797dbf09bea
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2018-05-15 19:59:36 -0700

    add get recent visitors example
    also doesn't work

---
+++ examples/visitors/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config({path: '../.env'});
+
+const OKCupid = require('okcupidjs');
+const okc = new OKCupid();
+
+okc.login(process.env.OKC_USER, process.env.OKC_PASS, function(err, res, body) {
+    okc.getVisitors((err, res, body) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log(body);
+        }
+    });
+});
